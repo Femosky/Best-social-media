8211fa0f040548bd8a3c38d7f123220249e16d24
Femosky <olufemiojeyemi@gmail.com>
2023-10-23 14:50:35 +0100

    still on navbar & routing

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export function Navbar() {
     return (
-        <div>
+        <div className="flex justify-between bg-green-300">
             <div>
                 <a href="/">
-                    <img src="../assets/logo-best.png" alt="" />
+                    <img className="border" src="../assets/logo-best.png" alt="" />
                 </a>
             </div>
             <div>
-                <Link to="/"></Link>
+                <Link to="/">Home</Link>
             </div>
             <div>
                 <Button>Login</Button>
